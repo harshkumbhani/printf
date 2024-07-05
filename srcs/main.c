69e7aa0f8285f9409f305ed7bafcffe1459b7a2e
Harsh <Harshwut@icloud.com>
2024-07-05 13:21:49 +0530

    fix: norminette fixed for both files

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: harsh <harsh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:33:18 by hkumbhan          #+#    #+#             */
-/*   Updated: 2024/07/05 12:46:07 by harsh            ###   ########.fr       */
+/*   Updated: 2024/07/05 13:16:34 by harsh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@
 # define PARAM "this is output for a string"
 #endif
 
-int	main()
+int	main(void)
 {
-	int len[2];
+	int	len[2];
+
 	printf("\n----- PRINTF -----\n");
 	len[0] = printf(PARAM);
-	//len[0] = printf("This is a string %s", 123);
 	printf("\n----- FT_PRINTF -----\n");
 	len[1] = ft_printf(PARAM);
-	//len[1] = printf("This is a string %s", 123);
-	printf("\n\nlen of printf = %d\nlen of ftprintf = %d\n",len[0],len[1]);
+	printf("\n\nlen of printf = %d\nlen of ftprintf = %d\n", len[0], len[1]);
 }
+
+// Some prefined tests
+//len[0] = printf("This is a string %s", 123);
+//len[1] = printf("This is a string %s", 123);
--- srcs/test.c
+++ srcs/test.c
@@ -1,29 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harsh <harsh@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 13:08:25 by hkumbhan          #+#    #+#             */
+/*   Updated: 2024/07/05 13:21:06 by harsh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include	<unistd.h>
-#include 	<string.h>
-int main(int c, char**v)
+#include <unistd.h>
+#include <string.h>
+
+int	main(int c, char**v)
 {
-	int p = 0;
-	for (int i = 1; i < c; i++)
+	int	p;
+	int	i;
+
+	p = 0;
+	i = 1;
+	while (i < c)
 	{
-		p =0;
+		p = 0;
 		while (v[i][p] != 0)
 		{
-			write(1,&v[i][p],1);
+			write(1, &v[i][p], 1);
 			p++;
 		}
+		i++;
 	}
-	return(0);
+	return (0);
 }
 
-
-int main(int c, char**v)
-{
-	int p = 0;
-	for (int i = 2; i < c; i++)
-	{
-			write(1,v[i],strlen(v[i]));
-			p++;
-	}
-	return(0);
-}
+// int main(int c, char**v)
+// {
+// 	int p = 0;
+// 	for (int i = 2; i < c; i++)
+// 	{
+// 			write(1,v[i],strlen(v[i]));
+// 			p++;
+// 	}
+// 	return(0);
+// }
