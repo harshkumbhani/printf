6fc70a568702ee9aedeb260b46eced9b4d82d886
Harsh <Harshwut@icloud.com>
2024-07-05 12:52:55 +0530

    update: main to use with test PARAM from makefile

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: harsh <harsh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:33:18 by hkumbhan          #+#    #+#             */
-/*   Updated: 2024/07/05 12:14:03 by harsh            ###   ########.fr       */
+/*   Updated: 2024/07/05 12:46:07 by harsh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,3 @@ int	main()
 	//len[1] = printf("This is a string %s", 123);
 	printf("\n\nlen of printf = %d\nlen of ftprintf = %d\n",len[0],len[1]);
 }
-
-/*
-0
-Hello World
-this is output for %%
-
-
-
-*/
